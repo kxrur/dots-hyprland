a0c158b7d71574de073c93877d977a7febcf4421
kxr <kxrury@gmail.com>
2026-06-11 19:33:41 -0400

    cursor

---
+++ dots/.config/hypr/custom/env.lua
@@ -0,0 +1,2 @@
+hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Classic")
+hl.env("HYPRCURSOR_SIZE", "16")
--- dots/.config/hypr/custom/execs.lua
+++ dots/.config/hypr/custom/execs.lua
@@ -3,8 +3,6 @@
 hl.on("hyprland.start", function ()
     -- exec-once = kanshi reload
     -- Cursor
-    hl.exec_cmd("hyprctl setcursor Bibata-Modern-Classic 20")
-
     -- exec-once = hyprpm reload -n // FIXME
     -- exec-once=hyprctl plugin load /home/kxr/Desktop/git/split-monitor-workspaces/split-monitor-workspaces.so
 
