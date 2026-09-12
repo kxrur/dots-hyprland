554540d930d794fdb2e5ab3c676a940faa1013ee
kxr <kxrury@gmail.com>
2026-09-12 06:20:17 -0400

    theme: current matugen/dms colors

--- dots/.config/hypr/hyprland/colors.lua
+++ dots/.config/hypr/hyprland/colors.lua
@@ -1,16 +1,16 @@
 hl.config({
     general = {
         col = {
-            active_border   = "rgba(44464f77)",
-            inactive_border = "rgba(1a1b2033)",
+            active_border   = "rgba(49454e77)",
+            inactive_border = "rgba(1d1b2033)",
         },
     },
     misc = {
-        background_color = "rgba(121318FF)",
+        background_color = "rgba(141218FF)",
     },
 })
 
-hl.window_rule({ -- not sure how to syntax "pin 1"
+hl.window_rule({
     match        = { pin = 1 },
-    border_color = "rgba(afc6ffAA) rgba(afc6ff77)",
+    border_color = "rgba(d0bcffAA) rgba(d0bcff77)",
 })
